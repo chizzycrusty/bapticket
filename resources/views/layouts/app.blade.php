df0076007ec223ef4b9bba96d75fb058c4c113b3
nem95 <naiim.chatti@gmail.com>
2016-04-19 23:08:07 +0200

    modification du code et correction des conflits

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,7 +112,7 @@
 
 
     @yield('content')
-
+    <footer> @include('layouts.footer') </footer>
             <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
